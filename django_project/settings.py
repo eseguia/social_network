4b2cd5663c6906c681015b2e24323d9b0660aadc
eseguia <emilio.eguia@gmail.com>
2020-12-10 20:16:36 -0300

    updated 2.0

--- django_project/settings.py
+++ django_project/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'users.apps.UsersConfig',
     'blog.apps.BlogConfig',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',  
@@ -120,3 +121,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL = 'blog-home'
+LOGIN_URL = 'login'
--- users/views.py
+++ users/views.py
@@ -1,9 +1,28 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
+from django.contrib.auth.decorators import login_required
 
 def register(request):
 	if request.method == 'POST':
-		form = UserCreationForm(request.POST)
+		form = UserRegisterForm(request.POST)
+		if form.is_valid():
+			form.save()
+			username = form.cleaned_data.get('username')
+			messages.success(request, 'Your account has been created! You are able to login')
+			return redirect('login')
 	else:
-		form = UserCreationForm()
+		form = UserRegisterForm()
 	return render(request, 'users/register.html', {'form':form})
+
+@login_required
+def profile(request):
+	u_form = UserUpdateForm()
+	p_form = ProfileUpdateForm()
+
+	context ={
+		'u_form':u_form,
+		'p_form':p_form
+	}
+
+	return render(request, 'users/profile.html', context)
