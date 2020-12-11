ed8237f9d3543725b5974873404b4f2efc3a6b77
eseguia <emilio.eguia@gmail.com>
2020-12-11 17:48:29 -0300

    .

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.models import User
 from django.views.generic import ListView,DetailView,CreateView, UpdateView, DeleteView
 from .models import Post
 
@@ -14,6 +15,19 @@ class PostListView(ListView):
 	template_name = 'blog/home.html'
 	context_object_name = 'posts'
 	ordering = ['-date']
+	paginate_by = 7
+
+
+class UserPostListView(ListView):
+	model = Post
+	template_name = 'blog/user_posts.html'
+	context_object_name = 'posts'
+	paginate_by = 7
+
+	def get_queryset(self):
+		user = get_object_or_404(User, username = self.kwargs.get('username'))
+		return Post.objects.filter(author=user).order_by('-date')
+
 
 class PostDetailView(DetailView):
 	model = Post
