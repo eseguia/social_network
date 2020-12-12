9b4ce3c3e145238f93c3d9e16d672e91260dae89
eseguia <emilio.eguia@gmail.com>
2020-12-11 21:02:59 -0300

    passw

--- django_project/settings.py
+++ django_project/settings.py
@@ -129,3 +129,8 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'
+
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
